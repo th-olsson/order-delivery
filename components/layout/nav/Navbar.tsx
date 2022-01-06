7f40596dbd5e483eed64f22f54217e16ec2dba77
Thomas Olsson <olss.thom@gmail.com>
2022-01-06 21:37:53 +0100

    adds icon buttons for menu and cart

--- components/layout/nav/Navbar.tsx
+++ components/layout/nav/Navbar.tsx
@@ -1,10 +1,21 @@
-import { Box } from '@chakra-ui/react';
+import { Box, HStack, IconButton } from '@chakra-ui/react';
 import NavLink from './NavLink';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { FaShoppingBasket } from 'react-icons/fa'
 
 function Nav() {
   return (
-    <Box bg="yellow.100">
-      <NavLink href="/" text="Home" />
+    <Box bg="yellow.100"
+      display="flex"
+      justifyContent="space-between"
+      alighItems="center"
+    >
+      <HStack>
+        <IconButton aria-label="open menu" icon={<AiOutlineMenu />} />
+        <NavLink href="/" text="Home" />
+      </HStack>
+
+      <IconButton aria-label="open cart" icon={<FaShoppingBasket />} />
     </Box>
   );
 }
