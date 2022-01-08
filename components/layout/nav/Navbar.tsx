4dbd5801bc549644cd8c086ab1263f0f8c19b665
Thomas Olsson <olss.thom@gmail.com>
2022-01-08 15:13:26 +0100

    cleans leftover stuff

--- components/layout/nav/Navbar.tsx
+++ components/layout/nav/Navbar.tsx
@@ -1,21 +1,19 @@
-import { Box, HStack, IconButton } from '@chakra-ui/react';
+import { Box, HStack } from '@chakra-ui/react';
 import NavLink from './NavLink';
-import { FaShoppingBasket } from 'react-icons/fa'
 import Menu from './Menu';
+import Cart from './Cart';
 
 function Nav() {
   return (
     <Box bg="yellow.100"
       display="flex"
       justifyContent="space-between"
-      alighItems="center"
     >
       <HStack>
         <Menu />
         <NavLink href="/" text="Home" />
       </HStack>
-
-      <IconButton aria-label="open cart" icon={<FaShoppingBasket />} />
+      <Cart />
     </Box>
   );
 }
