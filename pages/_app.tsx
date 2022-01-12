b5c471d94b63c10b45c2b8a38d05cff3445efb11
Thomas Olsson <olss.thom@gmail.com>
2022-01-12 04:06:44 +0100

    apollo provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from "@chakra-ui/react"
-import Layout from '../components/layout/Layout'
+import { ApolloProvider } from '@apollo/client'
+import apolloClient from 'lib/apolloClient'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <ApolloProvider client={apolloClient}>
         <Component {...pageProps} />
+      </ApolloProvider>
     </ChakraProvider>
   )
 }
