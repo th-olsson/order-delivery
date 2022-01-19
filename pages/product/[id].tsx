cb73ecf8f214319d61452d0a0771df1e26a3b073
Thomas Olsson <olss.thom@gmail.com>
2022-01-19 11:51:28 +0100

    test cart button w/ toast

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -3,8 +3,24 @@ import NextLink from "next/link";
 import apolloClient from "lib/apolloClient";
 import { GetAllProductsQuery, GetSingleProductQuery } from "lib/graphql/generated";
 import { GET_SINGLE_PRODUCT, GET_ALL_PRODUCTS } from "lib/graphql/queries";
+import { Button, useToast } from "@chakra-ui/react";
 
 function SingleProduct({ product }: GetSingleProductQuery) {
+  const addCartToast = useToast();
+  function addToCart() {
+    // Add to local storage
+    
+
+    // Display a toast message
+    addCartToast({
+      title: `${product?.name} has been added to your cart.`,
+      description: "Go to checkout.",
+      status: 'success',
+      duration: 9000,
+      isClosable: true,
+    });
+  }
+
   return (
     <>
       <NextLink href="/" passHref>
@@ -13,11 +29,15 @@ function SingleProduct({ product }: GetSingleProductQuery) {
       <h2>{product?.name}</h2>
       <p>{product?.price} sek</p>
       <p>{product?.description}</p>
+      <Button>Order now</Button>
+      <Button onClick={addToCart}>Add to cart</Button>
     </>
   )
 }
 
 export async function getStaticPaths() {
+  // TODO: Change to GET_PRODUCTS_SLUGS
+
   const { data: { products } } = await apolloClient.query<GetAllProductsQuery>({
     query: GET_ALL_PRODUCTS,
   });
