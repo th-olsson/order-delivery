06745e0f265cdf5e91a5c21fb47ce36346cd3a95
Thomas Olsson <olss.thom@gmail.com>
2022-01-27 23:24:10 +0100

    removes adyen and storybook stuff

--- stories/Layout/Nav/Navbar.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import Navbar from '../../../components/layout/nav/Navbar';
-
-export default {
-  title: "Layout/Nav/Navbar",
-  component: Navbar,
-} as ComponentMeta<typeof Navbar>;
-
-export const NavbarStory: ComponentStory<typeof Navbar> = () => <Navbar />
