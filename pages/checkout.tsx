4df215968b59a0bf8f594aa3c959a2f470730dc6
Thomas Olsson <olss.thom@gmail.com>
2022-01-27 19:46:15 +0100

    basic checkout design

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -5,7 +5,7 @@ import {
 } from 'react';
 import Image from 'next/image';
 // import { loadStripe } from '@stripe/stripe-js';
-import { Button, HStack, useToast } from '@chakra-ui/react';
+import { Box, Button, Container, HStack, useToast, VStack, Text, Divider } from '@chakra-ui/react';
 import { CartContext } from 'contexts/CartContext';
 
 function StripeCheckout() {
@@ -29,13 +29,13 @@ function StripeCheckout() {
   // });
 
   const checkOut = async () => {
-    const totalPrice = cart.reduce((acc, item) => acc + item.price, 0);
+    const totalPrice = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
 
     if (totalPrice <= 0) {
       toast({
-        title: 'Cart is empty',
-        description: 'Please add some items to your cart',
-        status: 'error',
+        title: 'Varukorgen är tom',
+        description: 'Lägg till något i varukorgen för att kunna göra en köp.',
+        status: 'info',
         isClosable: true,
       });
       return;
@@ -63,33 +63,63 @@ function StripeCheckout() {
   // const stripe = loadStripe(publishableKey);
 
   return (
-    <>
-      <h1>This is the checkout page</h1>
+    <Box
+      borderRadius='3xl'
+      ml='5'
+      mr='5'
+      pl='15'
+      pr='15'
+      pt='4rem'
+    >
+      <Container>
+        <VStack>
+          {
+            cart.map(({ id, name, price, quantity, imageUrl }) => (
+              <VStack key={id} w='100%'>
+                <HStack>
+                  <Text fontSize='lg' fontWeight='medium' fontStyle='italic' >{name}</Text>
+                  <Text fontSize='lg' fontWeight='medium' fontStyle='italic' >{price} kr</Text>
+                </HStack>
+                <HStack justifyContent='space-evenly' w='100%'>
+                  <Image
+                    height="120"
+                    width="120"
+                    src={`${process.env.NEXT_PUBLIC_HOST_URL}${imageUrl}`}
+                    alt={`${name} product image`}
+                  />
+                  <HStack>
+                    <Button onClick={() => removeItem(id)}>-</Button>
+                    <Text fontWeight='semibold'>{quantity}</Text>
+                    <Button onClick={() => addItem(id, name, price, imageUrl)}>+</Button>
+                  </HStack>
+                </HStack>
+                <Divider />
+              </VStack>
+            ))
+          }
 
-      {
-        cart.map(({ id, name, price, quantity, imageUrl }) => (
-          <div key={id}>
-            <Image
-              height="100"
-              width="100"
-              src={`${process.env.NEXT_PUBLIC_HOST_URL}${imageUrl}`}
-              alt={`${name} product image`}
-            />
-            <p>{name}</p>
-            <p>{price}</p>
-            <HStack>
-              <Button onClick={() => addItem(id, name, price, imageUrl)}>Add</Button>
-              <p>{quantity}</p>
-              <Button onClick={() => removeItem(id)}>Remove</Button>
-            </HStack>
-          </div>
-        ))
-      }
-
-      <p>Total: {cart.reduce((acc, item) => acc + item.price * item.quantity, 0)}</p>
-
-      <Button onClick={checkOut}>Checkout</Button>
-    </>
+        </VStack>
+        <VStack pb='2'>
+          <HStack>
+            <Text fontSize='2xl' fontWeight='semibold'>Totalt</Text>
+            <Text fontSize='2xl' fontWeight='bold'>
+              {cart.reduce((acc, item) => acc + item.price * item.quantity, 0)} kr
+            </Text>
+          </HStack>
+          <Button
+            onClick={checkOut}
+            size='lg'
+            rounded='sm'
+            bgColor='green.300'
+            _hover={
+              { bgColor: 'green.200' }
+            }
+          >
+            Betala
+          </Button>
+        </VStack>
+      </Container>
+    </Box>
   );
 }
 
